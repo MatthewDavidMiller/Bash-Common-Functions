2876bad0855c2fd0cee191cb0d67f8f1217050c4
MatthewDavidMiller <MatthewMiller@mdmiller.dev>
2021-02-14 00:30:37 -0500

    Finalizing apt_auto_updates.py and configure_ssh.py

--- functions/apt_auto_updates.py
+++ functions/apt_auto_updates.py
@@ -13,19 +13,19 @@ import re
 
 def apt_configure_auto_updates(release_name):
 
-    update_regex = str(r'Unattended-Upgrade::Origins-Pattern {.*};')
+    update_regex = str(r'Unattended-Upgrade::Origins-Pattern {' + '.*' + r'};')
 
     update_replace = str(r'Unattended-Upgrade::Origins-Pattern {' + '\n' + r'"origin=Debian,n=' + release_name + r',l = Debian";' + '\n' +
                          r'"origin=Debian,n=' + release_name + r',l = Debian-Security";' '\n' r'"origin=Debian,n=' +
                          release_name + r'-updates";' + '\n' + r'};')
 
-    reboot_regex = str(r'.*Unattended-Upgrade::Automatic-Reboot.*')
+    reboot_regex = str('.*' + r'Unattended-Upgrade::Automatic-Reboot' + '.*')
 
     reboot_replace = str(
         r'Unattended-Upgrade::Automatic-Reboot "true";')
 
     reboot_time_regex = str(
-        r'.*Unattended-Upgrade::Automatic-Reboot-Time.*')
+        '.*' + r'Unattended-Upgrade::Automatic-Reboot-Time' + '.*')
 
     reboot_time_replace = str(
         r'Unattended-Upgrade::Automatic-Reboot-Time "04:00";')
@@ -39,10 +39,10 @@ def apt_configure_auto_updates(release_name):
             opened_file.write(
                 re.sub(update_regex, update_replace, line, flags=re.S))
 
-            if (update_replace) == line.strip():
+            if update_replace == line.strip():
                 break
-            else:
-                opened_file.write(update_replace + '\n')
+        else:
+            opened_file.write(update_replace + '\n')
 
     # Allow reboots
     with open('/etc/apt/apt.conf.d/50unattended-upgrades', "r") as opened_file:
@@ -52,10 +52,10 @@ def apt_configure_auto_updates(release_name):
         for line in lines:
             opened_file.write(re.sub(reboot_regex, reboot_replace, line))
 
-            if (reboot_replace) == line.strip():
+            if reboot_replace == line.strip():
                 break
-            else:
-                opened_file.write(reboot_replace + '\n')
+        else:
+            opened_file.write(reboot_replace + '\n')
 
     # Set reboot time
     with open('/etc/apt/apt.conf.d/50unattended-upgrades', "r") as opened_file:
@@ -66,7 +66,7 @@ def apt_configure_auto_updates(release_name):
             opened_file.write(
                 re.sub(reboot_time_regex, reboot_time_replace, line))
 
-            if (reboot_time_replace) == line.strip():
+            if reboot_time_replace == line.strip():
                 break
-            else:
-                opened_file.write(reboot_time_replace + '\n')
+        else:
+            opened_file.write(reboot_time_replace + '\n')
--- functions/configure_ssh.py
+++ functions/configure_ssh.py
@@ -17,6 +17,28 @@ def configure_ssh():
 
     password_authentication_replace = str(r'PasswordAuthentication no')
 
+    empty_password_regex = str('.*' + r'PermitEmptyPasswords' + '.*')
+
+    empty_password_replace = str(r'PermitEmptyPasswords no')
+
+    disable_pam_regex = str('.*' + r'UsePAM' + '.*')
+
+    disable_pam_replace = str(r'UsePAM no')
+
+    disable_root_ssh_regex = str('.*' + r'PermitRootLogin' '.*')
+
+    disable_root_ssh_replace = str(r'PermitRootLogin no')
+
+    set_key_folder_regex = str('.*' + r'AuthorizedKeysFile' + '\s*' +
+                               r'.ssh/authorized_keys' + '\s*' + r'.ssh/authorized_keys2')
+
+    set_key_folder_replace = str(r'AuthorizedKeysFile .ssh/authorized_keys')
+
+    enable_pub_key_authentication_regex = str(
+        '.*' + r'PubkeyAuthentication' + '.*')
+
+    enable_pub_key_authentication_replace = str(r'PubkeyAuthentication yes')
+
     # Turn off password authentication
     with open('/etc/ssh/sshd_config', "r") as opened_file:
         lines = opened_file.readlines()
@@ -35,55 +57,56 @@ def configure_ssh():
         lines = opened_file.readlines()
     with open('/etc/ssh/sshd_config', "w") as opened_file:
         for line in lines:
-            opened_file.write(re.sub(".*PermitEmptyPasswords.*",
-                                     "PermitEmptyPasswords no", line))
-            if 'PermitEmptyPasswords no' == line.strip():
+            opened_file.write(re.sub(empty_password_regex,
+                                     empty_password_replace, line))
+            if empty_password_replace == line.strip():
                 break
         else:
-            opened_file.write('PermitEmptyPasswords no' + '\n')
+            opened_file.write(empty_password_replace + '\n')
 
 # Turn off PAM
     with open('/etc/ssh/sshd_config', "r") as opened_file:
         lines = opened_file.readlines()
     with open('/etc/ssh/sshd_config', "w") as opened_file:
         for line in lines:
-            opened_file.write(re.sub(".*UsePAM.*", "UsePAM no", line))
-            if 'UsePAM no' == line.strip():
+            opened_file.write(
+                re.sub(disable_pam_regex, disable_pam_replace, line))
+            if disable_pam_replace == line.strip():
                 break
         else:
-            opened_file.write('UsePAM no' + '\n')
+            opened_file.write(disable_pam_replace + '\n')
 
 # Turn off root ssh access
     with open('/etc/ssh/sshd_config', "r") as opened_file:
         lines = opened_file.readlines()
     with open('/etc/ssh/sshd_config', "w") as opened_file:
         for line in lines:
-            opened_file.write(re.sub(".*PermitRootLogin.*",
-                                     "PermitRootLogin no", line))
-            if 'PermitRootLogin no' == line.strip():
+            opened_file.write(re.sub(disable_root_ssh_regex,
+                                     disable_root_ssh_replace, line))
+            if disable_root_ssh_replace == line.strip():
                 break
         else:
-            opened_file.write('PermitRootLogin no' + '\n')
+            opened_file.write(disable_root_ssh_replace + '\n')
 
 # Enable public key authentication
     with open('/etc/ssh/sshd_config', "r") as opened_file:
         lines = opened_file.readlines()
     with open('/etc/ssh/sshd_config', "w") as opened_file:
         for line in lines:
-            opened_file.write(re.sub(".*AuthorizedKeysFile\s*\.ssh/authorized_keys\s*\.ssh/authorized_keys2",
-                                     'AuthorizedKeysFile .ssh/authorized_keys', line))
-            if 'AuthorizedKeysFile .ssh/authorized_keys' == line.strip():
+            opened_file.write(re.sub(set_key_folder_regex,
+                                     set_key_folder_replace, line))
+            if set_key_folder_replace == line.strip():
                 break
         else:
-            opened_file.write('AuthorizedKeysFile .ssh/authorized_keys' + '\n')
+            opened_file.write(set_key_folder_replace + '\n')
 
     with open('/etc/ssh/sshd_config', "r") as opened_file:
         lines = opened_file.readlines()
     with open('/etc/ssh/sshd_config', "w") as opened_file:
         for line in lines:
-            opened_file.write(re.sub(".*PubkeyAuthentication.*",
-                                     'PubkeyAuthentication yes', line))
-            if 'PubkeyAuthentication yes' == line.strip():
+            opened_file.write(re.sub(enable_pub_key_authentication_regex,
+                                     enable_pub_key_authentication_replace, line))
+            if enable_pub_key_authentication_replace == line.strip():
                 break
         else:
-            opened_file.write('PubkeyAuthentication yes' + '\n')
+            opened_file.write(enable_pub_key_authentication_replace + '\n')
